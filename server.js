108a9e2c7e5c4c74da86314bbef0236df6a6c043
Sridatta Thatipamala <sthatipamala@gmail.com>
2013-01-30 18:38:41 -0800

    change express constructor to be 2.x compat

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
 var express = require('express');
 
-var app = express();
+var app = express.createServer();
 
-var async = require('async');
-
-
-var http = require('http');
-var server = http.createServer(app);
-var sio = require('socket.io');
-var io = sio.listen(server);
-
+var fileUpload = require('./controllers/file-upload.js');
+var web = require('./controllers/web.js');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-
 app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
-
-
-
-
-
 
 
 app.post('/uploadDataSetToStorage', fileUpload.uploadDataSetToStorage);
@@ -29,10 +17,12 @@ app.post('/addDataSetFromHotlink', fileUpload.addDataSetFromHotlink);
 app.post('/modifyMetaData', fileUpload.modifyMetaData);
 
 
-app.post('/findDataSets', function(req, res){
-    
-});
+app.post('/findDataSets', function(req, res){ });
 
-app.post('/getDataSet', function(req, res){
-    
-});
+app.post('/getDataSet', function(req, res){ });
+
+app.get('/', web.home);
+app.get('/sets/create', web.create);
+app.get('/user/login', web.login);
+
+app.listen(3000);
