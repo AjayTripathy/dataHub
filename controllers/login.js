4723e3d28aa6f5c78b6ddf99e25c5fb7e32ec066
Ajay Tripathy <aurotripathy@Mackenzie.local>
2013-01-31 03:02:44 -0800

    added some login middleware

--- controllers/login.js
+++ controllers/login.js
@@ -3,10 +3,37 @@ var ObjectID = require('mongoskin').ObjectID;
 var db = mongo.db('localhost:27017/dataHub?auto_reconnect', {safe: true});
 var Users = db.collection('Users');
 
+exports.checkPermission = function(permission){
+	var userLoggedIn = function(req, res, next){
+		if ( ! req.session.user){
+			res.redirect('/login')
+		}
+		else if (req.session.user && (! req.session.user.permissions[permission])){
+			res.send(403);
+		}
+		else if (req.session.user && req.session.user.permissions[permission]){
+			next();
+		}
+		
+	}
+	return userLoggedIn;
+}
+
+
 exports.login = function(req, res){
 	//TODO: password shit
+	if (!req.form.isValid) {
+      res.send({'status': 'error', 'msg': 'fill out the forms completely'});
+      return;
+    }
 	Users.findOne({email: req.form.email}, function(err, result){
-		result._id = result._id.toString();
-		req.session.user = doc;
+		if(!doc || hash.sha256(req.form.password, doc.salt) !== doc.password) {
+        	res.send({'status': 'error', 'msg': 'invalid username/password combo'});
+      	} 
+      	else{
+			result._id = result._id.toString();
+			req.session.user = doc;
+			res.redirect('/dashboard');
+		}
 	});
 };
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var form = require('express-form'),
+    field = form.field;
 var app = express.createServer();
 var async = require('async');
 var mongo = require('mongoskin');
 var db = mongo.db('localhost:27017/dataHub?auto_reconnect', {safe: true});
 var MongoStore = require('connect-mongodb');
 var fileUpload = require('./controllers/file-upload');
+var login = require('./controllers/login');
 
 var http = require('http');
 //var server = http.createServer(app);
@@ -20,26 +23,21 @@ app.use(express.bodyParser());
 app.use(express.cookieParser());
 app.use(express.session({ secret: '#yolo', store: new MongoStore({db: db.db}), maxAge : new Date(Date.now() + 2628000000)}));
 
-var checkPermission = function(permission){
-	var userLoggedIn = function(req, res, next){
-		if ( ! req.session.user){
-			res.redirect('/login')
-		}
-		else if (req.session.user && (! req.session.user.permissions[permission])){
-			res.send(403);
-		}
-		else if (req.session.user && req.session.user.permissions[permission]){
-			next();
-		}
-		
-	}
-	return userLoggedIn;
-}
 
-app.post('/uploadDataSetToStorage', checkPermission('hostedUpload'), fileUpload.uploadDataSetToStorage);
-app.post('/addDataSetFromHotlink', checkPermission('hotlinkUpload'), fileUpload.addDataSetFromHotlink);
+
+app.post('/uploadDataSetToStorage', login.checkPermission('hostedUpload'), fileUpload.uploadDataSetToStorage);
+app.post('/addDataSetFromHotlink', login.checkPermission('hotlinkUpload'), fileUpload.addDataSetFromHotlink);
+
+app.post('/login', form(
+    			field('email').required('Email', 'Please enter an email').toLower().trim().isEmail('Email address is not valid'), 
+    			field('password').required('Password', 'Please enter a password')
+  			), 
+  			login.login
+);
 
 app.post('/modifyMetaData', fileUpload.modifyMetaData);
+
+
 
 
 app.post('/findDataSets', function(req, res){
