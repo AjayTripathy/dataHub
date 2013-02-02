60677d82d5ef36905522c6b35e4ce732b1c03791
Ajay Tripathy <aurotripathy@Mackenzie.local>
2013-02-01 23:46:17 -0800

    oh god more conflicts

--- controllers/search.js
+++ controllers/search.js
@@ -3,6 +3,49 @@ var ObjectID = require('mongoskin').ObjectID;
 var db = mongo.db('localhost:27017/dataHub?auto_reconnect', {safe: true});
 var MetaData = db.collection('MetaData');
 
+var parseQueryList = function(str){
+	return str.split('\u2603');
+}
+
+var find = function(query){
+	var keys = Object.keys(query);
+	var searchObj = []
+	for(var i = 0; i < keys.length; i = i + 1){
+		key = keys[i];
+		var miniQuery = {};
+		var lst  = parseQueryList(query[key])
+		if (lst.length > 1){
+			if (key == 'tags' || key = 'columns'){
+				miniQuery[key] = { '$all': lst};
+			}
+			else{
+				miniQuery[key] = {'$in': lst}
+			}
+		}
+		else{
+			miniQuery[key] = {lst[0]}
+		}
+		searchObj.push(miniQuery);
+
+	}
+	var toFind = {$and: searchObj}
+	return toFind;
+}
+
+exports.search = function(req, res){
+	var query = req.query;
+	toSearch = find(query)
+	MetaData.find(toSearch).toArray(function(err,docs){
+		for(var i = 0; i < docs.length; i = i + 1){
+			var doc = docs[i];
+			doc._id = doc._id.toString();
+		}
+		res.send({'status': 'ok', 'result': docs});
+	});
+}
+
+/*
+
 exports.findByColumnNames = function(req, res){
 	colNames = req.colNames;
 
@@ -14,11 +57,26 @@ exports.findByColumnNames = function(req, res){
 	});
 }
 
+*/
+
+
 exports.findByTitle = function(req, res){
+
 	
 }
 
+
+
 exports.findByTags = function(req, res){
+	tags = req.tags;
+
+	MetaData.find({ { 'tags': { '$all': tags} } ).toArray(function(err, docs){
+		if (err){
+			throw err;
+		}
+		res.send({'status': 'ok', 'data': docs});
+	});
+
 	
 }
 
