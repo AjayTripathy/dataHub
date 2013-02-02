c74da3bd4b03a920f34fd121e8b0af6280a064f8
Sridatta Thatipamala <sthatipamala@gmail.com>
2013-02-02 00:08:25 -0800

    hide login/register links in nav if logged in

--- controllers/web.js
+++ controllers/web.js
@@ -4,17 +4,21 @@ var db = mongo.db('localhost:27017/dataHub?auto_reconnect', {safe: true});
 var MetaData = db.collection('MetaData');
 
 exports.home = function(req, res) {
-  res.render('home');
+  var loggedIn = !!req.session.user;
+  res.render('home', {loggedIn: loggedIn});
 };
 
 exports.create = function(req, res) {
-  res.render('create');
+  var loggedIn = !!req.session.user;
+  res.render('create', {loggedIn: loggedIn});
 };
 
 exports.register = function(req, res) {
-  res.render('register');
+  var loggedIn = !!req.session.user;
+  res.render('register', {loggedIn: loggedIn});
 };
 
 exports.login = function(req, res) {
-  res.render('login');
+  var loggedIn = !!req.session.user;
+  res.render('login', {loggedIn: loggedIn});
 };
